010cd3798109a29257d7289571dfae166655902d
max_glb <tpztpzsc@gmail.com>
2021-06-06 15:15:54 +0200

    V 9,7,9 Report_Iteration_sql

---
+++ src/Resources/SQL/Data.sql
@@ -0,0 +1,12 @@
+-- Database: TourPlanner
+
+-- DROP DATABASE "TourPlanner";
+
+CREATE DATABASE "TourPlanner"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Chinese (Simplified)_China.1252'
+    LC_CTYPE = 'Chinese (Simplified)_China.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;

60e11eece86f490a9352563d6cf9fd650ac34f36
max_glb <tpztpzsc@gmail.com>
2021-06-07 15:49:28 +0200

    V 10,9,9 formal edition

--- test/BusinessLayer/ImExportManagerTest.java
+++ test/BusinessLayer/ImExportManagerTest.java
@@ -24,32 +24,37 @@ class ImExportManagerTest {
         tourinformanager.settour("Tour 3");
         logsinformanager logsinformanager=new logsinformanager(tourinformanager.TourId);
         logsinformanager.setLogslist();
-        Msg=ImExportManager.BuildJson(tourinformanager.tour,logsinformanager.Logslist);
 
-        System.out.println(Msg);
-       // Msg="[{\"Id\":\"67f9048f-99b8-4ae4-b866-d8008d00c53d\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"aa9999a0-734c-49c6-8f4a-651864b14e62\", \"Name\":\"RegularSpell\", \"Damage\": 50.0}, {\"Id\":\"d6e9c720-9b5a-40c7-a6b2-bc34752e3463\", \"Name\":\"Knight\", \"Damage\": 20.0}, {\"Id\":\"02a9c76e-b17d-427f-9240-2dd49b0d3bfd\", \"Name\":\"RegularSpell\", \"Damage\": 45.0}, {\"Id\":\"2508bf5c-20d7-43b4-8c77-bc677decadef\", \"Name\":\"FireElf\", \"Damage\": 25.0}]";
+            Msg = ImExportManager.BuildJson(tourinformanager.tour, logsinformanager.Logslist);
 
-       // System.out.println(Msg);
-        List<JsonMsg> jsonMsgArrayList= JsonParse.getjsonlist(Msg);
+            System.out.println(Msg);
+            if(Msg!="[") {
+            // Msg="[{\"Id\":\"67f9048f-99b8-4ae4-b866-d8008d00c53d\", \"Name\":\"WaterGoblin\", \"Damage\": 10.0}, {\"Id\":\"aa9999a0-734c-49c6-8f4a-651864b14e62\", \"Name\":\"RegularSpell\", \"Damage\": 50.0}, {\"Id\":\"d6e9c720-9b5a-40c7-a6b2-bc34752e3463\", \"Name\":\"Knight\", \"Damage\": 20.0}, {\"Id\":\"02a9c76e-b17d-427f-9240-2dd49b0d3bfd\", \"Name\":\"RegularSpell\", \"Damage\": 45.0}, {\"Id\":\"2508bf5c-20d7-43b4-8c77-bc677decadef\", \"Name\":\"FireElf\", \"Damage\": 25.0}]";
+
+            // System.out.println(Msg);
+            List<JsonMsg> jsonMsgArrayList = JsonParse.getjsonlist(Msg);
 
          /*for(int n=0;n<jsonMsgArrayList.size();n++){
              System.out.println("\nID: "+jsonMsgArrayList.get(n).getId()+" name: "+jsonMsgArrayList.get(n).getName()
                                    +" Damage: "+jsonMsgArrayList.get(n).getDamage()+"\n");
                        }*/
 
-        System.out.println(jsonMsgArrayList.get(0).getTourName());
-        System.out.println(jsonMsgArrayList.get(0).getDeparture());
-        System.out.println(jsonMsgArrayList.get(0).getDestination());
-        System.out.println(jsonMsgArrayList.get(0).getDescription()+"\n");
+            System.out.println(jsonMsgArrayList.get(0).getTourName());
+            System.out.println(jsonMsgArrayList.get(0).getDeparture());
+            System.out.println(jsonMsgArrayList.get(0).getDestination());
+            System.out.println(jsonMsgArrayList.get(0).getDescription() + "\n");
 
 
-        for (int i=0;i<jsonMsgArrayList.size();i++){
-            System.out.println(jsonMsgArrayList.get(i).getLogDate());
-            System.out.println(jsonMsgArrayList.get(i).getLogtime());
-            System.out.println(jsonMsgArrayList.get(i).getRating());
-            System.out.println(jsonMsgArrayList.get(i).getWeather());
-            System.out.println(jsonMsgArrayList.get(i).getDistance());
-        }
+            for (int i = 0; i < jsonMsgArrayList.size(); i++) {
+                System.out.println(jsonMsgArrayList.get(i).getLogDate());
+                System.out.println(jsonMsgArrayList.get(i).getLogtime());
+                System.out.println(jsonMsgArrayList.get(i).getRating());
+                System.out.println(jsonMsgArrayList.get(i).getWeather());
+                System.out.println(jsonMsgArrayList.get(i).getDistance());
+            }
+        }else {
+                System.out.println("No Tour");
+            }
     }
 
 }
